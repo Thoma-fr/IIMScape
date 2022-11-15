b927988c1d61a7f313b49928d8b18d841fec5377
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-11-15 08:54:23 +0100

    maj scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,13 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Camera cam;
+    public Transform target;
+
+    public GameObject vcam;
+    [SerializeField] private float distanceToTarget = 10;
+    public static RoomCam instance;
+    private Vector3 previousPosition;
     public GameObject roomCam;
     // Start is called before the first frame update
     void Start()
@@ -15,18 +23,46 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         if (Input.GetButton("Fire1"))
         {
             Vector3 Mousepos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z);
             Ray ray = Camera.main.ScreenPointToRay(Mousepos);
             Debug.DrawRay(Camera.main.transform.position, ray.direction * 1000, Color.red, 5f);
-            RaycastHit gunhit;
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, ray.direction, out hit, 100))
+            {
+                
+                RoomCam.instance.target = hit.transform;
+                RoomCam.instance.GetComponent<CinemachineVirtualCamera>().Priority = 20;
+            }
         }
-       
-        //if (Physics.Raycast(Camera.main.transform.position, ray.direction, out gunhit, range, mask))
-        //{
-            
-        //}
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            RoomCam.instance.GetComponent<CinemachineVirtualCamera>().Priority = 1;
+        }
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 direction = previousPosition - newPosition;
+
+            float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+            float rotationAroundXAxis = direction.y * 180; // camera moves vertically
+
+            vcam.transform.position = target.position;
+
+            vcam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+            vcam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
+
+            vcam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+            previousPosition = newPosition;
+        }
     }
 }
---
+++ Assets/Scripts/RoomCam.cs
@@ -0,0 +1,50 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCam : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    public Transform target;
+    [SerializeField] private float distanceToTarget = 10;
+    public static RoomCam instance;
+    private Vector3 previousPosition;
+
+    private void Awake()
+    {
+        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 direction = previousPosition - newPosition;
+
+            float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+            float rotationAroundXAxis = direction.y * 180; // camera moves vertically
+
+            transform.position = target.position;
+
+            transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+            transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
+
+            transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+            previousPosition = newPosition;
+        }
+    }
+}
