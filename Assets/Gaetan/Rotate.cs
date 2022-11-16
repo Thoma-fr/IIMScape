0a270928a594d980e916a7c053ca0e93013f57c6
GPM9747 <perre.gaetan@gmail.com>
2022-11-16 11:54:49 +0100

    Rotate.cs update + updated LVL_Space01

--- Assets/Gaetan/Rotate.cs
+++ Assets/Gaetan/Rotate.cs
@@ -9,5 +9,10 @@ public class Rotate : MonoBehaviour
     void Update()
     {
         transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
+        if (transform.rotation.y>360)
+        {
+            Vector3 rot = new Vector3(transform.rotation.x, 0, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rot);
+        }
     }
 }
