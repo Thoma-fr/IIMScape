b3e6e1642a0e274481c9f06a9c906298c6d6a27b
GPM9747 <perre.gaetan@gmail.com>
2022-11-17 11:41:06 +0100

    Main Menu V1

---
+++ Assets/Gaetan/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+        Debug.Log(volume);
+    }
+
+    public void StartSceneByIndex(int p_index)
+    {
+        SceneManager.LoadScene(p_index);
+    }
+
+    public void StartSceneByName(string p_name)
+    {
+        SceneManager.LoadScene(p_name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
